f65d03e14373f5b399e652a9780d3e36a5d7757b
cameronpaschal <cpaschy@byui.edu>
2025-11-24 20:46:58 -0700

    updated user service and created methods to do most user functionality

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -19,14 +19,32 @@
      
 from backend.repositories.user_repository import UserRepository
 from backend.infrastructure.security import Passwords
+from typing import Optional, List
+import datetime
+from email_validator import validate_email, EmailNotValidError
      
 class UserService:
     
     def __init__(self, db):
         self._ur=UserRepository(db)
         self._pw=Passwords()
+        
+        
     
-    async def register_user(self, username: str, email: str, display_name: str, job_title: str, password:str, bio: str = None) -> str:
+    async def get_all_users(self) -> List[dict]:
+        return await self._ur.find_all()
+    
+    async def get_user_by_id(self, id: int) -> dict:
+        return await self._ur.find_by_id(id)
+    
+    async def delete_user_by_id(self, deleter_id: int, id_to_be_deleted: int) -> str:
+        if deleter_id == id_to_be_deleted:
+            raise ValueError("A user cannot delete themselves")
+        
+        return await self._ur.delete_user(id_to_be_deleted)
+    
+    
+    async def register_user(self, username: str, email: str, display_name: str, job_title: str, password:str, bio: Optional[str] = None) -> str:
         email = email.strip().lower()
         username = username.strip().lower()
  
@@ -44,10 +62,46 @@ class UserService:
         
         return await self._ur.create_user(username, email, display_name, job_title, pw_hashed, bio)
         
-    # async def update_profile
-    # async def update_password
-    # async def update_username
-    # async def update_email
+    async def update_profile(self, id: int, new_display_name: Optional[str] = None, new_bio: Optional[str] = None, new_job_title: Optional[str] = None) -> str:
+        
+        return await self._ur.update_user(id,
+		new_display_name=new_display_name,
+		new_bio=new_bio,
+		new_job_title=new_job_title
+		)
+        
+        
+        
+    async def update_password(self, id: int, new_password: str) -> str:
+        self._validate_password(new_password)
+        
+        new_password = self._pw.hash_pw(new_password)
+        
+        return await self._ur.update_user(id, new_pw_hash=new_password)
+    
+
+    async def update_username(self, id: int, new_username: str) -> str:
+        new_username = self._validate_username(new_username)
+        
+        if await self._ur.find_by_username(new_username):
+            raise ValueError("Username already taken")
+        
+        return await self._ur.update_user(id, new_username=new_username)
+    
+    
+    async def update_email(self, id: int, new_email: str) -> str:
+        new_email = new_email.strip().lower()
+        try:
+            valid = validate_email(new_email)
+            normalized = valid.email
+            
+            if await self._ur.find_by_email(normalized):
+                raise ValueError("Email already taken")
+            
+            return await self._ur.update_user(id, new_email=normalized)
+        
+        except EmailNotValidError as e:
+            raise ValueError(str(e))
     
     
     
@@ -73,7 +127,7 @@ class UserService:
         
         
     def _validate_username(self, username: str) -> None:
-        username = username.strip().lower()
+        username = username.strip()
         
         if len(username) < 3:
             raise ValueError("Username must be at least 3 characters long.")
@@ -84,3 +138,6 @@ class UserService:
         allowed = set("abcdefghijklmnopqrstuvwxyz0123456789_.")
         if any(c not in allowed for c in username):
             raise ValueError("Username may only contain lowercase letters, digits, \'.\', and \'_\'.")
+         
+        username = username.lower()
+        return username
