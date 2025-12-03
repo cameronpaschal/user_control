c0bb68620d103926262415558450960ce729d8e5
cameronpaschal <cpaschy@byui.edu>
2025-12-03 01:29:18 -0700

    made sure the live server can make requests

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from backend.server.controllers.user_controller import router as user_router
@@ -21,6 +21,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",   # your frontend
+        "http://localhost:3000",   # if you use React dev server later
+        "http://localhost:8081",   # Expo web
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(auth_router)
